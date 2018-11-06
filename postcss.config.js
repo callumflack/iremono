7e7d977ea04b2200e382a042ee39e27c11aeb0d5
Callum Flack <callumflack@gmail.com>
2018-11-06 12:54:07 +1000

    update: test use of postcss config via iremono node_module

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+module.exports = {
+  map: true,
+  plugins: {
+    "postcss-easy-import": {},
+    "postcss-custom-properties": {},
+    "postcss-mixins": {},
+    "postcss-custom-selectors": {},
+    "postcss-nested": {},
+    autoprefixer: {},
+    "postcss-custom-media": {},
+    "postcss-discard-comments": {}
+  }
+};
