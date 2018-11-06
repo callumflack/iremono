10315c8841bede4959239da7ff994b1034bfa2b9
Callum Flack <callumflack@gmail.com>
2018-11-06 13:29:02 +1000

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,8 @@ module.exports = {
   map: true,
   plugins: {
     "postcss-easy-import": {},
-    "postcss-custom-properties": {},
     "postcss-mixins": {},
-    "postcss-custom-selectors": {},
-    "postcss-nested": {},
-    autoprefixer: {},
-    "postcss-custom-media": {},
+    "postcss-preset-env": { stage: 0 },
     "postcss-discard-comments": {}
   }
 };
